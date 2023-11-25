f2ed0c5d53443e5d318a7a79f9759ceb3abae23e
xiften <6wn5seb9@anonaddy.me>
2023-11-25 13:13:49 -0700

    added search: NO DEBOUNCE

---
+++ src/components/search.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import React from "react";
+import { Input } from "./ui/input";
+
+//FIX: Add debounce
+export default function Search() {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  function handleChange(v: string) {
+    if (v == "" || v == " ") {
+      router.push(pathname);
+    }
+    router.push(`${pathname}/?name=${v}`);
+  }
+
+  return (
+    <div className="w-1/2">
+      <Input
+        className="h-6 md:h-12"
+        onChange={(event) => handleChange(event.target.value)}
+      />
+    </div>
+  );
+}
