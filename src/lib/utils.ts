352230aee126bbdb3e27a8fc91b9711627924bc2
xiften <6wn5seb9@anonaddy.me>
2023-11-25 13:14:01 -0700

    added filterColumns

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,13 @@
 import { type ClassValue, clsx } from "clsx";
+import {
+  eq,
+  like,
+  not,
+  notLike,
+  type Column,
+  type ColumnBaseConfig,
+  type ColumnDataType,
+} from "drizzle-orm";
 import { toast } from "sonner";
 import { twMerge } from "tailwind-merge";
 import { z } from "zod";
@@ -25,4 +34,27 @@ export function slugify(str: string) {
     .toLowerCase()
     .replace(/[^\w ]+/g, "")
     .replace(/ +/g, "-");
+}
+
+export function filterColumn({
+  column,
+  value,
+}: {
+  column: Column<ColumnBaseConfig<ColumnDataType, string>, object, object>;
+  value: string;
+}) {
+  const [filterValue, filterVariety] = value?.split(".") ?? [];
+
+  switch (filterVariety) {
+    case "contains":
+      return like(column, `%${filterValue}%`);
+    case "does not contain":
+      return notLike(column, `%${filterValue}%`);
+    case "is":
+      return eq(column, filterValue);
+    case "is not":
+      return not(eq(column, filterValue));
+    default:
+      return like(column, `%${filterValue}%`);
+  }
 }
