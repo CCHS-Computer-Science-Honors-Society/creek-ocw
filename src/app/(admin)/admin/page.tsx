40e57652b1824f398ba101942fd9bce8b18f8ef1
xiften <108333567+not-ani@users.noreply.github.com>
2023-12-23 15:18:20 -0700

    chore: memoized getUser function

--- src/app/(admin)/admin/page.tsx
+++ src/app/(admin)/admin/page.tsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { cache } from "react";
 import { z } from "zod";
 import Search from "./search";
 import EmailSearch from "./EmailSearch";
 import { api } from "@/trpc/server";
 import { UserForm } from "./UserForm";
+import { db } from "@/server/db";
 
 const searchParamsSchema = z.object({
   name: z.string().optional(),
   email: z.string().optional(),
 });
+
+async function getData() {
+  const data = await db.query.course.findMany({
+    columns: {
+      id: true,
+      name: true,
+    },
+  });
+
+  return data;
+}
+
+const fetch = cache(getData);
+const fetchUser = cache(api.admin.getUser.query);
 
 export default async function Page(props: {
   searchParams: Record<string, string | string[] | undefined>;
@@ -16,17 +31,19 @@ export default async function Page(props: {
   const { searchParams } = props;
   const { name, email } = searchParamsSchema.parse(searchParams);
 
-  const data = await api.admin.getUser.query({
+  const data = await fetchUser({
     name: name,
     email: email,
   });
+
+  const courses = await fetch();
 
   return (
     <div className="flex flex-col p-20">
       <SearchBar />
       <div className="flex flex-col p-10">
         {data.map((user) => {
-          return <UserForm data={user} key={user.id} />;
+          return <UserForm courses={courses} data={user} key={user.id} />;
         })}
       </div>
     </div>
